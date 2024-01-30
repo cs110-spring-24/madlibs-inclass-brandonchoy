6b50338859cb41b2b4d92e843fd929b2d0f83a2c
brandon <brandonchoy05@gmail.com>
2024-01-29 16:43:37 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,18 @@
 # TODO Create your inputs, variables, and add your story here!
+
+noun = input("Enter a noun: ")
+adj = input("Enter a adjective: ")
+verb = input("Enter a verb: ")
+
+input1 = input("Enter in anything: ")
+input2 = input("Enter in anything: ")
+input3 = input("Enter in anything: ")
+input4 = input("Enter in anything: ")
+input5 = input("Enter in anything: ")
+input6 = input("Enter in anything: ")
+input7 = input("Enter in anything: ")
+input8 = input("Enter in anything: ")
+input9 = input("Enter in anything: ")
+input10 = input("Enter in anything: ")
+
+print(f"My brand new {noun} was very {adj}, it was {verb}. {input1} was just like {input3}. But the other day, {input2} had gone to {input4}. Going back to before, it is similar to {input5} which also is like {input6} to some extent. {input7} was pretty crazy and so was {input8}, lets not forget about {input9} and {input10}")
